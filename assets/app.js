c1776122b27ee6cc0c8fcedc7ee4f23b3c6e2b70
michal <sadsad@fsad.pl>
2023-08-06 15:53:43 +0200

    Add tooltips to buttons

--- assets/app.js
+++ assets/app.js
@@ -4,6 +4,8 @@ import React from 'react';
 import Home from "./home";
 
 import { createRoot } from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(<Home />);
